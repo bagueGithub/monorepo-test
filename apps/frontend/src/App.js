8a96371e7da482a5409fd87434b886d2ac578561
bagueGithub <bague.github@gmail.com>
2023-02-27 15:46:11 +0800

    Test commit

--- apps/frontend/src/App.js
+++ apps/frontend/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 // Import the shared function into the `create-react-app` application
-import { getSiteInfo } from 'site-info';
+// import { getSiteInfo } from 'site-info';
 
 export default function App() {
   //Define a variable to get the values
@@ -10,8 +10,9 @@ export default function App() {
     <div className="App">
       {/*Output the site title and subtitle to the screen*/}
       <header className="App-header">
-        <h1>{siteInfo.title}</h1>
-        <p>{siteInfo.subtitle}</p>
+        <p>This is app1</p>
+        {/* <h1>{siteInfo.title}</h1> */}
+        {/* <p>{siteInfo.subtitle}</p> */}
       </header>
     </div>
   );
